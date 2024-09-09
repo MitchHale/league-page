b6129c1128a96c55f047c2142cb5c8a7f04c0d5b
Mitchell Hale <Mitchhale7@gmail.com>
2024-09-09 17:12:02 -0400

    Updating Manager info

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -62,8 +62,8 @@ export const managers = [
        "favoriteTeam": "atl", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
-         name: "Chayce", // Can be anything (usually your rival's name)
-         link: 6, // manager array number within this array, or null to link back to all managers page
+         name: "Chayce Horton", // Can be anything (usually your rival's name)
+         link: 1, // manager array number within this array, or null to link back to all managers page
          image: "/managers/horton1.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
        },
        "favoritePlayer": 947, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
